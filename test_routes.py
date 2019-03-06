5733a8b042e2f380dbff8f8eead02ddb6c184e33
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-06 20:23:25 +1300

    need db test and func

--- test_routes.py
+++ test_routes.py
@@ -21,3 +21,24 @@ class TestContainerOnHome(unittest.TestCase):
     def test_homepage(self):
         rv = self.app.get('/home')
         self.assertIn('Welcome to the Homepage', rv.data)
+
+
+class DBFunctions(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_container(self):
+        rv = self.app.get('/')
+        self.assertIn('container', rv.data)
+
+    def test_blogs(self):
+        rv = self.app.get('/blogs')
+        self.assertIn('Welcome to blogs', rv.data)
+
+    def test_about(self):
+        rv = self.app.get('/about')
+        self.assertIn('Welcome to About', rv.data)
+
+    def test_homepage(self):
+        rv = self.app.get('/home')
+        self.assertIn('Welcome to the Homepage', rv.data)
