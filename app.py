6959b3ca340c070b6a34f486697de200b15e5866
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-06 19:53:19 +1300

    render template tests

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/home')
+def home():
+    return render_template('main.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ test_routes.py
@@ -0,0 +1,23 @@
+import unittest
+import app
+
+
+class TestContainerOnHome(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_container(self):
+        rv = self.app.get('/')
+        self.assertIn('container', rv.data)
+
+    def test_blogs(self):
+        rv = self.app.get('/blogs')
+        self.assertIn('Welcome to blogs', rv.data)
+
+    def test_about(self):
+        rv = self.app.get('/about')
+        self.assertIn('Welcome to About', rv.data)
+
+    def test_homepage(self):
+        rv = self.app.get('/home')
+        self.assertIn('Welcome to the Homepage', rv.data)
